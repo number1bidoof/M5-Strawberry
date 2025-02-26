377151fdc1d31b7edfa21398c9b55c7781afa780
number1bidoof <anabdalla1@my.waketech.edu>
2025-02-26 12:13:21 -0500

    Done with Assigment
    :)

--- chapter/js03_txt.js
+++ chapter/js03_txt.js
@@ -9,31 +9,57 @@
 
      Filename: js03.js
  */
-//days of the week
-let WeekDays = ["Sundays", "Mondays", "Tuesday","Wednesday",
-    "Thursday","Friday","Saturday","Sunday"]
-window.addEventListener('load', addWeekdays)
- function addWeekdays(){
-    let i = 0;
-    let headingCells = document.getElementsByTagName('th');
-    while(i>7) {
-        headingCells[i].innerHTML = WeekDays[i]
-        i++
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+window.addEventListener("load", weekDays);
+function addWeekdays(){
+    let i = 0;                     
+    let headingCells = document.getElementsByTagName("th");
+    while (i<7){
+        headingCells[i].innerHTML= weekDays[i]
+        i ++;
     }
 
- }
+}
 
- window.addEventListener('load',showGames)
- function showGames(){
-    for(let i = 0; i < gameDates.length; i++){
-        let gamesInfo = ""
-        gamesInfo += "<p>"
-        gamesInfo += gameOpponents[i] +"<br>"
-        gamesInfo += gameResults[i] + ": (" + runsScored[i] + "-" + runsAllowed[i] + ")";
-        gamesInfo += "</p>"
-        let tableCell = document.getElementsById(gameDates[i])
-        tableCell.insertAdjacentHTML("beforeEnd", gamesInfo)
+window.addEventListener("load", showGames);
 
+function showGames(){
+    for (let i = 0; i<gameDates.length; i++){
+        let gameInfo = "";
+        switch (gameResults[i]) {
+            case "W":
+                gameInfo += "<p class='win'>";
+                break;
+            case "L":
+                gameInfo += "<p class='lose'>";
+                break;
+            case "S":
+                gameInfo += "<p class='suspended'>";
+                break;
+            case "P":
+                gameInfo += "<p class='postponed'>";
+                break;
+        }
+        if (gameLocations[i] === "h") {
+            gameInfo += "vs. ";
+        }
+        else if (gameLocations[i] === "a"){
+            gameInfo += "@ ";
+        }
+        gameInfo += gameOpponents[i] + "<br>";
+        gameInfo += gameResults[i] + ": (" + runsScored[i] + " - " + runsAllowed[i] + ")";
+        if (gameInnings[i] < 5) {
+            gameInfo += " [" + gameInnings[i] + "]***";
+        }
+        else if (gameInnings[i] < 9) {
+            gameInfo += " [" + gameInnings[i] + "]*";
+        }
+        else if (gameInnings[i] > 9){
+            gameInfo += " [" + gameInnings[i] + "]";
+        }
+        gameInfo += "</p>";
+        let tableCell = document.getElementById(gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo)
     }
- }
- 
+}
